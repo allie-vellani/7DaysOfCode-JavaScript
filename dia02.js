094c005769e8a755f02b2207934840a30bd9caf0
allie-vellani <allie.vellani@outlook.com>
2025-02-27 14:17:30 -0300

    Resolução do Dia 02 do #7DaysOfCode

---
+++ dia02.js
@@ -0,0 +1,6 @@
+let nome = prompt('Qual é o seu nome?');
+let idade = prompt('Quantos anos você tem?');
+let linguagem = prompt('Qual linguagem de programação você está estudando?');
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}. Que ótimo!`)
+
